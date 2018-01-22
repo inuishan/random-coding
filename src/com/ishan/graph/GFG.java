3fdaae8f1f7466cedbcbc7002f9c7f5fad59d2a6
ishan <ishanjain1991@gmail.com>
2018-01-22 22:51:28 +0530

    initial commit

---
+++ src/com/ishan/graph/GFG.java
@@ -0,0 +1,14 @@
+package com.ishan.graph;
+
+/**
+ * {@see <a link="https://practice.geeksforgeeks.org/problems/x-total-shapes/0"> X total shapes</a>}
+ *
+ * @author ishanjain
+ * @since 22/01/18
+ */
+public class GFG {
+
+    public static void main(String[] args) {
+
+    }
+}
